2b86e9fe05761ec2cce6dfb44497fc8042de406c
Michael S. Baek <seungjinbaek@Seungjins-MBP.home>
2015-08-04 14:36:39 -0700

    lesson 7 files

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,6 +19,7 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
@@ -26,7 +27,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+# clf = svm.SVC(kernel = 'linear')
+clf = svm.SVC(kernel = 'rbf',C = 10000.0)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time() - t1, 3), "s"
+
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+#print acc
+
+#print pred[10], pred[26], pred[50]
 #########################################################
 
 
