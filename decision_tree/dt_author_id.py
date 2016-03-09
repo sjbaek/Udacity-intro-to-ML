faa6c85f68df173869bb6290994209f7d98fa8d7
Michael S. Baek <seungjinbaek@Seungjins-MacBook-Pro.local>
2016-03-09 10:16:23 -0800

    recent changes

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -28,10 +28,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn import tree
 clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+
+t0 = time()
 clf.fit(features_train,labels_train)
+print "training time:", round(time() - t0, 3), "s"
 
+t1 = time()
 pred = clf.predict(features_test)
+print "training time:", round(time() - t1, 3), "s"
 
+print len(features_train[0])
 from sklearn.metrics import accuracy_score
 print accuracy_score(pred, labels_test)
 #########################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,10 +28,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 from sklearn import svm
-# clf = svm.SVC(kernel = 'linear')
+#clf = svm.SVC(kernel = 'linear')
 clf = svm.SVC(kernel = 'rbf',C = 10000.0)
-#features_train = features_train[:len(features_train)/100]
-#labels_train = labels_train[:len(labels_train)/100]
+#clf = svm.SVC(kernel = 'rbf')
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 t0 = time()
 clf.fit(features_train,labels_train)
@@ -44,9 +45,10 @@ print "predict time:", round(time() - t1, 3), "s"
 
 from sklearn.metrics import accuracy_score
 acc = accuracy_score(pred, labels_test)
-#print acc
+print acc
 
 #print pred[10], pred[26], pred[50]
+print sum(pred)
 #########################################################
 
 
