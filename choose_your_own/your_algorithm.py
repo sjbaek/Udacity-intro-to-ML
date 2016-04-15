e6f940490fb1af41c98af7c8b4b19454a16ad3a9
Seungjin Baek <sjbaek@users.noreply.github.com>
2016-04-15 15:01:58 -0700

    Update your_algorithm.py
    detailed explanation on clf.fit and clf.predict

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -37,10 +37,16 @@ from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier(n_estimators = 100)
 
 # 그 오브젝트에 데이터를 입력함. (내부적으로 모델 parameter가 정의되는 듯)
+
+# clf는 원래 빈 껍데기 (그저 어떤 클래시파이어를 쓰겠다는 것만 정의 된) 였으나
+# .fit 과 주어진 train 데이터를 통해서 
+# 조율되어진 '함수'로 다시 태어난다 이말이지.
 clf.fit(features_train,labels_train)
 
 # 위에서 정의된 parameter를 토대로, 새로운 데이터 (feature_test)를 입력하면
 # 예측값 (=pred) 가 생성됨. 
+
+# clf가 train 데이터를 통해서 함수로 튜닝이 되었으므로, .predict를 이용해서 써먹는거야
 pred = clf.predict(features_test)
 
 from sklearn.metrics import accuracy_score
