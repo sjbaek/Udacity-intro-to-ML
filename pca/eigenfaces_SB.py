860baa1eed7a8f5f16b6a735ee888d3ef10d86bd
sjbaek <sjbaek@gmail.com>
2016-08-16 22:31:59 -0700

    feat: Try different number of Principal Component

--- pca/eigenfaces_SB.py
+++ pca/eigenfaces_SB.py
@@ -66,7 +66,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 250
+N_Components = [10,15,25,50,100,250]
+n_components = N_Components[1]
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
